6a33a10485457861673c799eca32ec7e78f22875
aj-codess <jagyei339@gmail.com>
2024-09-27 06:40:14 -0400

    progress

--- include/id_gen_connector.h
+++ include/id_gen_connector.h
@@ -57,6 +57,8 @@ void id_gen_con::server_init(){
 
             this->resolved_address=std::move(resolved_address);
 
+            if(this->resolved_address.empty())
+            cout<<"shid didnt work "<<endl;
         };
 
     });
@@ -65,43 +67,82 @@ void id_gen_con::server_init(){
 
 
 
-void id_gen_con::connect_init(){
+// void id_gen_con::connect_init(){
 
-    boost::asio::spawn(ioc,[this](boost::asio::yield_context yield_ioc){
-        this->io_pipe(yield_ioc);
-    });
+//     boost::asio::spawn(ioc,[this](boost::asio::yield_context yield_ioc){
+//         this->io_pipe(yield_ioc);
+//     });
 
-};
-
-
-void id_gen_con::io_pipe(boost::asio::yield_context yield_ioc){
-    this->stream_socket.expires_after(std::chrono::seconds(30));
-
-    struct con{
-        boost::beast::tcp_stream stream_socket;
-        boost::beast::flat_buffer buffer={};
-        boost::beast::http::request<boost::beast::http::string_body> req={};
-        boost::beast::http::response<boost::beast::http::string_body> res={};
-    };
-
-    try{
-
-        stream_socket.async_connect(this->resolved_address,yield_ioc,[this,yield_ioc](boost::beast::error_code ec,boost::asio::ip::tcp::resolver::results_type){
-
-            this->stream_socket.expires_after(std::chrono::seconds(30));
-
-            if(!ec){
+// };
 
 
+// void id_gen_con::io_pipe(boost::asio::yield_context yield_ioc){
 
-            } else{
-                cout<<"error connecting - "<<ec.message()<<endl;
-            };
+//     struct con{
+//         boost::beast::tcp_stream stream_socket;
+//         boost::beast::flat_buffer buffer={};
+//         boost::beast::http::request<boost::beast::http::string_body> req={};
+//         boost::beast::http::response<boost::beast::http::string_body> res={};
+//     };
 
-        });
+//     auto state=std::make_shared<con>(con{boost::beast::tcp_stream{boost::asio::make_strand(ioc)}});
 
-    } catch(const exception& e){
-        cout<<"Error Connecting - "<<e.what()<<endl;
-    };
+//     state->stream_socket.expires_after(std::chrono::seconds(30));
 
-};
+//     try{
+
+//     boost::asio::ip::tcp::endpoint endpoint_=*this->resolved_address.begin();
+
+//         state->stream_socket.async_connect(resolved_address,yield_ioc,
+//         [state](boost::beast::error_code ec,boost::asio::ip::tcp::resolver::results_type){
+
+//             state->stream_socket.expires_after(std::chrono::seconds(30));
+
+//             if(!ec){
+
+//                 boost::beast::http::async_write(state->stream_socket,state->req,[state](boost::beast::error_code ec,std::size_t size){
+//                     boost::ignore_unused(size);
+
+//                     if(ec){
+//                         cout<<"Error Writing - "<<ec.message()<<endl;
+//                         //retry
+//                     } else{
+
+//                         boost::beast::http::async_read(
+//                                     state->stream_socket, state->buffer, state->res,
+//                                     [state](boost::beast::error_code ec, size_t transfered_size) mutable {
+//                                         boost::ignore_unused(transfered_size);
+
+//                                         if (ec) {
+//                                             cout<<"error reading from remote Server - "<<ec.message()<<endl;
+//                                         };
+
+//                                     });
+
+//                                 if (state->req.need_eof()) {
+
+//                                     boost::beast::error_code shutdown_ec;
+
+//                                     state->stream_socket.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_send,
+//                                                                            shutdown_ec);
+
+//                                     if (shutdown_ec) {
+//                                         std::cout << "Error shutting down: " << shutdown_ec.message()
+//                                                   << std::endl;
+//                                     }
+//                                 };
+
+//                                 };
+//                 });
+
+//             } else{
+//                 cout<<"error connecting - "<<ec.message()<<endl;
+//             };
+
+//         });
+
+//     } catch(const exception& e){
+//         cout<<"Error Connecting - "<<e.what()<<endl;
+//     };
+
+// };
